3e8a171fa407ce3e4de7c73edb856b8111d81176
MicheliChaves <micheli.traderbr@gmail.com>
2021-06-26 23:15:52 -0300

    Adicionando ultima aula de API

--- objetos&funcoes/objectAPI.js
+++ objetos&funcoes/objectAPI.js
@@ -22,5 +22,44 @@ Object.defineProperty(objectJs, 'name', {
     value: 'Javascript',
 });
 
-console.log(objectJs);
-console.log(objectJs.name);
+console.log(objectJs); // não mostra a propriedade, pois quando definimos apenas o value todo o resto é considerado falso
+console.log(Object.keys(objectJs));// aparece a chave
+console.log(Object.values(objectJs));
+console.log(Object.entries(objectJs));
+
+//Definindo um objeto enumerábel com defineProperty
+const objectJs2 = {};
+Object.defineProperty(objectJs2, 'name', {
+    enumerable: true,
+    value: 'Javascript',
+});
+
+console.log(objectJs2);
+console.log(Object.entries(objectJs2));
+
+//Definindo se podemos ou não alterar valor de alguma propriedade.
+const objectJs3 = {};
+Object.defineProperty(objectJs3, 'name', {
+    enumerable: true,
+    writable: true,
+    value: 'Javascript',
+});
+
+objectJs3.name = 'ECMAScript';
+
+console.log(objectJs3);
+
+//Configurable: por padrão vem como falso, para apagar uma propriedade precisamos configurar para true
+const objectJs4 = {};
+Object.defineProperty(objectJs4, 'name', {
+    enumerable: true,
+    writable: true,
+    configurable: true,
+    value: 'Javascript',
+});
+
+objectJs4.name = 'ECMAScript';
+delete objectJs4.name;
+console.log(objectJs4);
+
+
---
+++ objetos&funcoes/objectAPI2.js
@@ -0,0 +1,59 @@
+
+//preventExtensions:  impede que objeto tenha novas propriedades, 
+//mas permite remover ou modificar as propriedades existentes.
+
+const javascript = {
+    name: 'Javascript',
+    year: 1995,
+    paradigm: 'OO and Functional',
+}
+
+Object.preventExtensions(javascript);
+javascript.name = 'ECMAScript';
+javascript.author = 'Brendon Eich'; //ignorado
+//delete javascript.year; possível
+//javascript.year = 2000; ; //possível
+console.log(javascript);
+console.log(Object.isExtensible(javascript)); //testando se o preventExtensios foi ativado.
+
+/*
+seal: impede que o objeto tenha novas propriedades ou apague propriedades existentes, 
+mas permite a modificação de propriedades existentes.
+*/
+
+const javascript2 = {
+    name: 'Javascript',
+    year: 1995,
+    paradigm: 'OO and Functional',
+}
+
+Object.seal(javascript2);
+javascript2.name = 'ECMAScript';
+//javascript2.author = 'Brendon Eich'; //ignorado
+//delete javascript.year; // ignorado
+//javascript.year = 2000; ; //possível
+console.log(javascript2);
+console.log(Object.isExtensible(javascript2)); //testando se o preventExtensios foi ativado.
+console.log(Object.isSealed(javascript2)); //testando se o seal foi ativado.
+
+const javascript3 =  {
+    name: 'Javascript',
+    year: 1995,
+    paradigm: 'OO and Functional',
+};
+
+Object.freeze(javascript3);
+
+javascript2.name = 'ECMAScript'; //ignorado
+//javascript2.author = 'Brendon Eich'; //ignorado
+//delete javascript.year; // ignorado
+//javascript.year = 2000; ; //ignorado
+
+console.log(Object.isFrozen(javascript3));
+
+
+
+
+
+
+
